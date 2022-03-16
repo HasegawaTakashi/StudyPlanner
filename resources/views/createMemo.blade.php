74f9de5d32b59d7e76d5b74b3a671c22d74c65e2
HasegawaTakashi <euphonium813@gmail.com>
2022-03-16 09:46:30 +0900

    ホームページに[メモ新規作成]ボタンを追加しました #22
    ログイン後のページに[メモ新規作成]ボタンを追加、
    ボタンを押下するとメモ作成ページ[createMemo.blade.php]に遷移します。

--- resources/views/createMemo.blade.php
+++ resources/views/createMemo.blade.php
@@ -11,6 +11,19 @@
 <body>
     <h1>Memo</h1>
     <p>CreateMemoのテストページです</p>
+
+    <form>
+        <div class="form-group">
+            <label for="title">タイトル</label>
+            <input type="text" class="form-control" id="title" name="title">
+            </div>
+        <div class="form-group">
+            <label for="memo">メモ</label>
+            <input type="text" class="form-control" id="memo" name="memo">
+            </div>
+        <a href="{{ route('dashboard') }}" class="btn btn-primary">戻る</a>
+        <button type="submit" class="btn btn-success">追加</button>
+        </form>
 </body>
 
 </html>
