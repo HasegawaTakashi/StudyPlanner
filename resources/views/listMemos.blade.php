4ef73f5c40cffdb3b6a81ec9ba637fdddadab5d6
HasegawaTakashi <euphonium813@gmail.com>
2022-03-19 12:11:39 +0900

    メモ一覧表示用のメモ表示枠を作成しました。
    一覧表示に使うメモ表示枠を作成しました。
    現状は1枠のみ、DBからデータを未取得状態です。

--- resources/views/listMemos.blade.php
+++ resources/views/listMemos.blade.php
@@ -11,7 +11,18 @@
 <body>
     <h1>listMemo</h1>
     <p>listMemoのテストページです</p>
-    
+
+    <form method="POST" action="{{ route('store') }}">
+        @csrf
+        <div class="form-group">
+            <label for="title">タイトル</label>
+            <input type="text" class="form-control" id="title" name="title">
+        </div>
+        <div class="form-group">
+            <textarea class="form-control" name="memo" cols="30" rows="10" placeholder="メモ"></textarea>
+        </div>
+        <a href="{{ route('dashboard') }}" class="btn">戻る</a>
+    </form>
 </body>
 
 </html>
