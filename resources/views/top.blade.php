ade5aadbdc7ed61fd9d7deff745de32c049fef2d
HasegawaTakashi <euphonium813@gmail.com>
2022-04-16 09:42:23 +0900

    [top]edit: メモ表示スペースを作成しました。

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -8,10 +8,11 @@
   <script src="{{ asset('js/app.js') }}"></script>
   <title>Top(bootstrap)</title>
 </head>
-<body style="background-color:#BFDBFE; --bg-opacity: 1; height: 100vh" class="container-fluid">
+<body style="background-color:#BFDBFE; --bg-opacity: 1; height: 100vh" class="container">
+  <div class="d-block">
     <div class="navbar d-flex justify-content-around">
       @auth
-        <button type="button" class="btn btn-secondary  btn-lg">
+        <button type="button" class="btn btn-secondary btn-lg">
           <a href="{{ url('/dashboard') }}" class="btn">Dashboard</a>
         </button>
       @else
@@ -25,6 +26,14 @@
           @endif
       @endauth
     </div>
-
+  </div>
+  <div class="card">
+    <div class="card-header text-center p-5">
+      メモタイトル表示スペース
+    </div>
+    <div class="card-body p-5">
+      <p class="card-text text-center">メモ内容表示スペース</p>
+    </div>
+  </div>
 </body>
 </html>
