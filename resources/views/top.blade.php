7e751ab42883d955f3694fa78789674b7e0f8ff6
HasegawaTakashi <euphonium813@gmail.com>
2022-04-18 08:23:15 +0900

    [top]edit: ボタンをnavbarでまとめました。

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -8,25 +8,35 @@
   <script src="{{ asset('js/app.js') }}"></script>
   <title>Top(bootstrap)</title>
 </head>
-<body style="background-color:#BFDBFE; --bg-opacity: 1; height: 100vh" class="container">
-  <div class="d-block">
-    <div class="navbar d-flex justify-content-around">
-      @auth
-        <button type="button" class="btn btn-secondary btn-lg">
-          <a href="{{ url('/dashboard') }}" class="btn">Dashboard</a>
-        </button>
-      @else
-        <button type="button" class="btn btn-secondary btn-lg">
-          <a href="{{ route('login') }}" class="btn">ログイン</a>
-        </button>
-          @if (Route::has('register'))
-          <button type="button" class="btn btn-secondary btn-lg">
-            <a href="{{ route('register') }}" class="btn">新規登録</a>
-          </button>
+<body style="background-color:#BFDBFE; --bg-opacity: 1; height: 100vh" class="container text-black-50">
+  <nav class="navbar navbar-expand-lg navbar-light list-unstyled my-3">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        @auth
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <button type="button" class="btn btn-secondary">
+                <a class="nav-link active text-light" aria-current="page" href="{{ url('/dashboard') }}">ホーム</a>
+              </button>
+            </li>
+          @else
+            <li class="nav-item">
+              <button type="button" class="btn btn-secondary">
+                <a class="nav-link text-light" href="{{ route('login') }}">ログイン</a>
+              </button>
+            </li>
+            @if (Route::has('register'))
+            <li class="nav-item ms-3">
+              <button type="button" class="btn btn-secondary">
+                <a class="nav-link text-light" href="{{ route('register') }}">新規登録</a>
+              </button>
+            </li>
+          </ul>
           @endif
-      @endauth
+        @endauth
+      </div>
     </div>
-  </div>
+  </nav>
+
   <div class="card">
     <div class="card-header text-center p-5">
       メモタイトル表示スペース
