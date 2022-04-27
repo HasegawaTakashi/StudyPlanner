8f7ae2796d65e76747689f048c77fc45418effa6
HasegawaTakashi <euphonium813@gmail.com>
2022-04-28 08:37:02 +0900

    [editMemos]edit: メモ編集ページのレイアウトを編集しました。

--- resources/views/editMemos.blade.php
+++ resources/views/editMemos.blade.php
@@ -1,48 +1,52 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+<html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EditMemo</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <script src="{{ asset('js/app.js') }}"></script>
+  <title>メモ編集</title>
 </head>
-<body>
-    <h1>Memo</h1>
-    <p>EditMemoのテストページです</p>
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
+<body style="background-color:#BFDBFE; --bg-opacity: 1; height: 100vh" class="container text-black-50">
+    <div class="m-3">
+        <h1>Memo</h1>
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
 
-    @if ($does_memo_exists === false)
-        <p>メモはありません</p>
-    @else
-        <ul>
-            <form method="post" action="{{ route('memo.update') }}">
-                @csrf
-                <div class="form-group">
-                    <label for="memo_id"></label>
-                    <input type="hidden" class="form-control" id="memo_id" name="memo_id" value="{{ $memo->id }}">
-                </div>
-                <div class="form-group">
-                    <label for="title">タイトル</label>
-                    <input type="text" class="form-control" id="title" name="title" value="{{ $memo->title }}">
-                </div>
-                <div class="form-group">
-                    <textarea class="form-control" name="memo" cols="30" rows="10" placeholder="メモ">{{ $memo->memo }}</textarea>
-                </div>
-                <button class="btn" type="submit">保存</button>
-            </form>
-        </ul>
-    @endif
-    <button class="btn"><a href="{{ route('dashboard') }}">戻る</a></button>
-    </form>
+        @if ($does_memo_exists === false)
+            <p>メモはありません</p>
+        @else
+            <ul>
+                <form method="post" action="{{ route('memo.update') }}">
+                    @csrf
+                    <div class="form-group">
+                        <label for="memo_id"></label>
+                        <input type="hidden" class="form-control" id="memo_id" name="memo_id" value="{{ $memo->id }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="title"></label>
+                        <input type="text" class="form-control" id="title" name="title" value="{{ $memo->title }}">
+                    </div>
+                    <div class="form-group">
+                        <textarea class="form-control" name="memo" cols="30" rows="10" placeholder="メモ">{{ $memo->memo }}</textarea>
+                    </div>
+                    <button class="btn btn-primary my-3" type="submit">保存</button>
+                </form>
+            </ul>
+        @endif
+        <div class="d-flex justify-content-end">
+            <button class="btn btn-success opacity-75"><a href="{{ route('dashboard') }}" class="text-black">戻る</a></button>
+        </div>
+        </form>
+    </div>
 </body>
 
 </html>
