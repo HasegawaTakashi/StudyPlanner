4978651415bb3fbc3215b398b4014edd83abfbe2
HasegawaTakashi <euphonium813@gmail.com>
2022-04-28 07:45:04 +0900

    [createMemo]edit: ブートストラップを適応してレイアウトを編集しました。

--- resources/views/createMemo.blade.php
+++ resources/views/createMemo.blade.php
@@ -1,37 +1,41 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+<html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <script src="{{ asset('js/app.js') }}"></script>
     <title>CreateMemo</title>
 </head>
-<body>
-    <h1>Memo</h1>
-    <p>CreateMemoのテストページです</p>
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
+<body style="background-color:#BFDBFE; --bg-opacity: 1; height: 100vh" class="container text-black-50">
+    <div class="my-3">
+        <h1>Memo</h1>
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul class="my-3">
+                @foreach ($errors->all() as $error)
+                <li class="list-unstyled fw-bold">{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
 
-    <form method="POST" action="{{ route('memo.store') }}">
-        @csrf
-        <div class="form-group">
-            <label for="title">タイトル</label>
-            <input type="text" class="form-control" id="title" name="title">
-        </div>
-        <div class="form-group">
-            <textarea class="form-control" name="memo" cols="30" rows="10" placeholder="メモ"></textarea>
-        </div>
-        <a href="{{ route('dashboard') }}" class="btn">戻る</a>
-        <button type="submit" class="btn">追加</button>
-    </form>
+        <form method="POST" action="{{ route('memo.store') }}">
+            @csrf
+            <div class="form-group">
+                <label for="title"></label>
+                <input type="text" class="form-control" id="title" name="title" placeholder="タイトル">
+            </div>
+            <div class="form-group">
+                <textarea class="form-control" name="memo" cols="30" rows="10" placeholder="メモ"></textarea>
+            </div>
+            <button type="submit" class="my-3 btn btn-sm btn-primary opacity-75">追加</button>
+            <button class="my-3 btn-sm btn btn-success opacity-75">
+                <a href="{{ route('home') }}" class="text-black">戻る</a>
+            </button>
+        </form>
+    </div>
 </body>
 
 </html>
