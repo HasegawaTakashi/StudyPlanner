7f135b51ae48c915dbfe1bdfebf2f03921340346
HasegawaTakashi <euphonium813@gmail.com>
2022-04-28 10:20:57 +0900

    [signIn]edit: タイトルを「なんでも学習帳 ログイン」へ編集しました。

--- resources/views/signIn.blade.php
+++ resources/views/signIn.blade.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   <script src="{{ asset('js/app.js') }}"></script>
-  <title>Document</title>
+  <title>なんでも学習帳 ログイン</title>
 </head>
 
 <body style="background-color:#BFDBFE; --bg-opacity: 1; height: 100vh" class="container text-black-50">
